0011d528a6b5479605260da55cfe758b6cc8db89
anush <anush@LAPTOP-HKKSME9N>
2021-05-27 21:25:59 -0400

    Practice

--- DISCLass2/Program.cs
+++ DISCLass2/Program.cs
@@ -6,7 +6,7 @@ namespace DISCLass2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Anusha!");
         }
     }
 }
